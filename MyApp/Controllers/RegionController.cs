759e8a2084dfa9fcedd29409c3f6416a44ee48b0
Yasin <yasinpeker43@gmail.com>
2025-10-02 15:38:23 +0300

    add RegionController

---
+++ MyApp/Controllers/RegionController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        
+    }
+}
